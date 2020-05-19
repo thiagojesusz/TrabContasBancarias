06974f16c2378bad1c53666fae897edf72a364d6
Thiago Silva de Jesus <thiagosilvaj7@gmail.com>
2020-05-19 12:07:28 -0300

    Projeto iniciado mostrando olá mundo

---
+++ ContasBancarias/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContasBancarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
